5b41a8a99611d9882a8e3ee8282de5a86af3f698
Greet Verduyn <greetverduyn@proximus.be>
2022-08-31 11:17:54 +0200

    made a template and called it in the method

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -18,14 +18,9 @@ class LearningController extends AbstractController
 
 
     #[Route ('/about-me')]
-    public function AboutMe(): Response{
-       return new Response(
-           '<html><body>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum."</body></html>'
-       );
+    public function AboutMe(): Response
+    {
+       return $this->render('example.html.twig', ['name' => 'BeCode']);
     }
 
 
