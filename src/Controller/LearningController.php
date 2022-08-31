5d8128ed9972ce86d377e0d8b908a159d3d61623
Greet Verduyn <greetverduyn@proximus.be>
2022-08-31 10:10:17 +0200

    made a controller and the method aboutMe

---
+++ src/Controller/LearningController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LearningController extends AbstractController
+{
+    #[Route('/learning', name: 'app_learning')]
+    public function index(): Response
+    {
+        return $this->render('learning/index.html.twig', [
+            'controller_name' => 'LearningController',
+        ]);
+    }
+
+
+    #[Route ('/about-me')]
+    public function AboutMe(): Response{
+       return new Response(
+           '<html><body>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum."</body></html>'
+       );
+    }
+
+
+
+
+
+}
