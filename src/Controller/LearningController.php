4646b8d117f6fc1730647440384fe7de91da82e8
Greet Verduyn <greetverduyn@proximus.be>
2022-09-01 14:22:11 +0200

    split function index and number

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -18,10 +18,27 @@ class LearningController extends AbstractController
 
 
     #[Route ('/about-me')]
-    public function AboutMe(): Response
+   public function number(): Response
+    {
+        return new Response(
+            '<html><body>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                    qui officia deserunt mollit anim id est laborum.
+                    </body></html>'
+        );
+    }
+
+    #[Route ('/homepage')]
+    public function showName(): Response
     {
         $name = 'Unknown';
-       return $this->render('example.html.twig', ['name' => $name]);
+        return $this->render('example.html.twig', [
+            'name' => $name
+        ]);
     }
 
 
