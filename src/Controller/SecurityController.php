cb6c0b7dd77fee18f24547fe032e7970056960c4
Greet Verduyn <greetverduyn@proximus.be>
2022-09-02 16:31:04 +0200

    add roles

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,6 +10,6 @@ class SecurityController extends AbstractController
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
     {
-        trow new\Exception
+        throw new\Exception;
     }
 }
