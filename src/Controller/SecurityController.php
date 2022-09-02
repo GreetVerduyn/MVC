b3c3dea728fd5b6aea84e583eacae5f3192c903b
Greet Verduyn <greetverduyn@proximus.be>
2022-09-02 14:52:04 +0200

    create logout

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,13 @@
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        trow new\Exception
+    }
+}
